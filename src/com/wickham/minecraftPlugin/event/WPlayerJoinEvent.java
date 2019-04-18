db4e1e1b26dd94eb53ae6a14311dc6cc49f16713
WickhamWei <895644173@qq.com>
2019-04-18 17:13:06 +0800

    修复玩家退出提示信息的字体颜色

--- src/com/wickham/minecraftPlugin/event/WPlayerJoinEvent.java
+++ src/com/wickham/minecraftPlugin/event/WPlayerJoinEvent.java
@@ -1,5 +1,6 @@
 package com.wickham.minecraftPlugin.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,7 +11,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.wickham.minecraftPlugin.WickhamsPlugin;
 
-public class WPlayerJoinEvent implements Listener{
+public class WPlayerJoinEvent implements Listener {
 	private boolean levelHealthBoolean;
 	private int maxHealth;
 	private boolean welcomeMsgBoolean;
@@ -20,13 +21,13 @@ public class WPlayerJoinEvent implements Listener{
 
 	public WPlayerJoinEvent(FileConfiguration mainConfiguration) {
 		levelHealthBoolean = mainConfiguration.getBoolean("三十级后每升级一级加血量上限");
-		welcomeMsgBoolean =mainConfiguration.getBoolean("玩家加入时给玩家的信息开关");
-		loginSystemBoolean=mainConfiguration.getBoolean("登陆系统");
-		versionBoolean=mainConfiguration.getBoolean("玩家加入时的插件介绍");
+		welcomeMsgBoolean = mainConfiguration.getBoolean("玩家加入时给玩家的信息开关");
+		loginSystemBoolean = mainConfiguration.getBoolean("登陆系统");
+		versionBoolean = mainConfiguration.getBoolean("玩家加入时的插件介绍");
 		maxHealth = mainConfiguration.getInt("最大血量上限，一颗心为两个血");
-		this.mainConfiguration=mainConfiguration;
+		this.mainConfiguration = mainConfiguration;
 	}
-	
+
 	@EventHandler
 	public void levelHealth(PlayerJoinEvent event) {
 		if (maxHealth < 20) {// 不设置小于20
@@ -47,25 +48,23 @@ public class WPlayerJoinEvent implements Listener{
 			player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(20);
 		}
 	}
-	
+
 	@EventHandler
 	public void welcomeMsg(PlayerJoinEvent event) {// 欢迎信息
-		if (welcomeMsgBoolean&&!loginSystemBoolean) {
+		event.setJoinMessage("");
+		if (welcomeMsgBoolean && !loginSystemBoolean) {
 			Player player = event.getPlayer();
-			player.sendMessage(ChatColor.GREEN+mainConfiguration.getString("玩家加入时给玩家的信息"));
-		}else
-			return;		
-	}
-	
-	@EventHandler
-	public void versionMsg(PlayerJoinEvent event) {// 欢迎信息
-		if(!loginSystemBoolean) {
-			event.setJoinMessage(ChatColor.GREEN+event.getPlayer().getName()+" 加入了游戏");
-			if (versionBoolean)
-				event.getPlayer().sendMessage(ChatColor.GREEN+("服务器基础插件由Wickham提供技术支持，插件版本号V"+WickhamsPlugin.getMain().getDescription().getVersion()));
-			return;	
-		}else {//由登录插件接管
-			event.setJoinMessage("");
+			player.sendMessage(ChatColor.GREEN + mainConfiguration.getString("玩家加入时给玩家的信息"));
 		}
+		if (!loginSystemBoolean) {
+			Bukkit.broadcastMessage(ChatColor.GREEN + event.getPlayer().getName() + " 加入了游戏");
+			if (versionBoolean)
+				event.getPlayer().sendMessage(ChatColor.GREEN
+						+ ("服务器基础插件由Wickham提供技术支持，插件版本号V" + WickhamsPlugin.getMain().getDescription().getVersion()));
+			return;
+		} else {// 由登录插件接管
+			;
+		}
+		return;
 	}
 }
--- src/com/wickham/minecraftPlugin/event/WPlayerQuitEvent.java
+++ src/com/wickham/minecraftPlugin/event/WPlayerQuitEvent.java
@@ -1,15 +1,17 @@
 package com.wickham.minecraftPlugin.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class WPlayerQuitEvent implements Listener {
-	
+
 	@EventHandler
 	public void versionMsg(PlayerQuitEvent event) {// 欢迎信息
-		event.setQuitMessage(ChatColor.YELLOW+event.getPlayer().getName()+" 退出了游戏");
-		return;		
+		event.setQuitMessage("");
+		Bukkit.broadcastMessage(ChatColor.YELLOW + event.getPlayer().getName() + " 退出了游戏");
+		return;
 	}
 }
