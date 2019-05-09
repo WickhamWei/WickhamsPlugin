23a039eae7b46d9eab3970d0784aebdb113cb3d6
895644173@qq.com <weichunhua@weichunhuadeMacBook-Pro.local>
2019-05-09 17:50:24 +0800

    插件启动时检查配置文件内容

--- src/wickhamsPlugin/WickhamsPlugin.java
+++ src/wickhamsPlugin/WickhamsPlugin.java
@@ -24,15 +24,21 @@ import wickhamsPlugin.recipe.HugeRottenFlash;
 import wickhamsPlugin.tpASystem.TpACommand;
 import wickhamsPlugin.tpASystem.TpACommandYes;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
 import org.bukkit.Server;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 
 public class WickhamsPlugin extends JavaPlugin implements Listener {
 
 	public static WickhamsPlugin MAIN;// 主类静态变量
-	public final FileConfiguration mainConfiguration = getConfig();//终态配置文件
-	public final Server mainServer = getServer();//终态服务器
+	public final FileConfiguration mainConfiguration = getConfig();// 终态配置文件
+	public final Server mainServer = getServer();// 终态服务器
 
 	@Override
 	public void onEnable() {// 插件启动
@@ -51,9 +57,29 @@ public class WickhamsPlugin extends JavaPlugin implements Listener {
 		this.saveDefaultConfig();// You can create a copy of config.yml from the jar into the plugin's data
 									// folder by invoking JavaPlugin's saveDefaultConfig() method.
 									// saveDefaultConfig() will not overwrite an existing file.
+
+		checkConfigurationSectionExist(mainConfiguration, "登陆系统");
+		checkConfigurationSectionExist(mainConfiguration, "登录保持时间");
+		checkConfigurationSectionExist(mainConfiguration, "自动保存地图");
+		checkConfigurationSectionExist(mainConfiguration, "自动保存地图时间间隔，单位分钟");
+		checkConfigurationSectionExist(mainConfiguration, "自动公告");
+		checkConfigurationSectionExist(mainConfiguration, "自动公告时间间隔，单位分钟");
+		checkConfigurationSectionExist(mainConfiguration, "公告1");
+		checkConfigurationSectionExist(mainConfiguration, "公告2");
+		checkConfigurationSectionExist(mainConfiguration, "公告3");
+		checkConfigurationSectionExist(mainConfiguration, "玩家加入时给玩家的信息开关");
+		checkConfigurationSectionExist(mainConfiguration, "玩家加入时给玩家的信息");
+		checkConfigurationSectionExist(mainConfiguration, "玩家加入时的插件介绍");
+		checkConfigurationSectionExist(mainConfiguration, "死亡是否保留背包内的物品");
+		checkConfigurationSectionExist(mainConfiguration, "死亡后保留一半等级");
+		checkConfigurationSectionExist(mainConfiguration, "三十级后每升级一级加血量上限");
+		checkConfigurationSectionExist(mainConfiguration, "最大血量上限");
+		checkConfigurationSectionExist(mainConfiguration, "保护耕地不被踩坏");
+		checkConfigurationSectionExist(mainConfiguration, "非OP传送等待时间（秒）");
+		checkConfigurationSectionExist(mainConfiguration, "tpa请求等待时间（秒）");
 		if (mainConfiguration.getBoolean("登陆系统")) {
 			LoginMain.createPlayerPasswordConfig();
-			LoginMain.copyOldPasswordFile();//⑨的登录系统配置文件迁移、加密、删除
+			LoginMain.copyOldPasswordFile();// ⑨的登录系统配置文件迁移、加密、删除
 		}
 	}
 
@@ -87,5 +113,43 @@ public class WickhamsPlugin extends JavaPlugin implements Listener {
 
 	public void loadRecipe() {// 读取新的合成表
 		mainServer.getPluginManager().registerEvents(new HugeRottenFlash(), this);
+	}
+
+	public static void checkConfigurationSectionExist(FileConfiguration configuration,
+			String configurationSectionString) {// 检查配置项
+		if (configuration.contains(configurationSectionString)) {
+			return;
+		} else {
+			WickhamsPlugin.MAIN.getLogger().log(Level.WARNING,
+					"设置项 " + configurationSectionString + " 不存在，请删除配置文件并重启服务器");
+			MAIN.getServer().shutdown();
+			return;
+		}
+	}
+
+	public static FileConfiguration createConfig(String fileNameString) {
+		File file = new File(MAIN.getDataFolder(), fileNameString + ".yml");
+		if (!file.exists()) {
+			MAIN.getLogger().log(Level.WARNING, fileNameString + " 文件不存在");
+			file.getParentFile().mkdirs();
+			MAIN.saveResource(fileNameString + ".yml", false);
+		}
+		YamlConfiguration config = new YamlConfiguration();
+		try {
+			config.load(file);
+		} catch (IOException | InvalidConfigurationException e) {
+			e.printStackTrace();
+			MAIN.getLogger().log(Level.WARNING, fileNameString + " 配置文件创建失败");
+		}
+		return config;
+	}
+
+	public static void saveConfig(FileConfiguration configuration, String fileNameString) {
+		try {
+			configuration.save(new File(MAIN.getDataFolder(), fileNameString + ".yml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			MAIN.getLogger().log(Level.WARNING, fileNameString + " 配置文件保存失败");
+		}
 	}
 }
