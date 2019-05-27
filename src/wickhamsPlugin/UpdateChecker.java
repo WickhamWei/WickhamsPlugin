22faddb85fb45fc8158a802efd1662e56ef3aeb8
WickhamWei <895644173@qq.com>
2019-05-27 16:06:00 +0800

    日常提交

---
+++ src/wickhamsPlugin/UpdateChecker.java
@@ -0,0 +1,9 @@
+package wickhamsPlugin;
+
+public class UpdateChecker {
+	String url = "https://api.github.com/repos/WickhamWei/WickhamsPlugin/releases/latest";
+
+	public UpdateChecker() {
+		
+	}
+}
