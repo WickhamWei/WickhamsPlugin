4881d0a61cebb0b3ad0e493767a4c6a2f97070fe
WickhamWei <895644173@qq.com>
2019-06-06 21:37:50 +0800

    日常提交

--- src/wickhamsPlugin/levelEntitySpawn/EntitySpawnEventListener.java
+++ src/wickhamsPlugin/levelEntitySpawn/EntitySpawnEventListener.java
@@ -12,12 +12,43 @@ public class EntitySpawnEventListener implements Listener {
 		if (event.getEntity() instanceof Zombie) {
 			Zombie targeEntity = (Zombie) event.getEntity();
 			double targeEntityMaxHeatlh = targeEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+			double targeEntityDamage = targeEntity.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).getBaseValue();
 			double X=targeEntity.getLocation().getX();
 			double Y=targeEntity.getLocation().getY();
-			if (X>=100&&X<200&&Y>=100&&Y<200) {
-				targeEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(targeEntityMaxHeatlh * 1.1);
-				return;
+			int locationChunk = 0;
+			if (X >= 100 || X <= -100 || Y >= 100 || Y <= -100) {
+				locationChunk = 1;
+				if (X >= 200 || X <= -200 || Y >= 200 || Y <= -200) {
+					locationChunk = 2;
+					if (X >= 300 || X <= -300 || Y >= 300 || Y <= -300) {
+						locationChunk = 3;
+						if (X >= 400 || X <= -400 || Y >= 400 || Y <= -400) {
+							locationChunk = 4;
+							if (X >= 500 || X <= -500 || Y >= 500 || Y <= -500) {
+								locationChunk = 5;
+								if (X >= 600 || X <= -600 || Y >= 600 || Y <= -600) {
+									locationChunk = 6;
+									if (X >= 700 || X <= -700 || Y >= 700 || Y <= -700) {
+										locationChunk = 7;
+										if (X >= 800 || X <= -800 || Y >= 800 || Y <= -800) {
+											locationChunk = 8;
+											if (X >= 900 || X <= -900 || Y >= 900 || Y <= -900) {
+												locationChunk = 9;
+												if (X >= 1000 || X <= -1000 || Y >= 1000 || Y <= -1000) {
+													locationChunk = 10;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
+			targeEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(targeEntityMaxHeatlh * (1+(locationChunk/10)));
+			targeEntity.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(targeEntityDamage * (1+(locationChunk/10)));
+			return;
 		}
 	}
 }
--- src/wickhamsPlugin/levelEntitySpawn/PlayerMoveEventListener.java
+++ src/wickhamsPlugin/levelEntitySpawn/PlayerMoveEventListener.java
@@ -15,21 +15,52 @@ public class PlayerMoveEventListener implements Listener {
 		if (LoginMain.loginSystemIsEnable()) {
 			if (LoginMain.isLogin(event.getPlayer())) {
 				Player targePlayer = event.getPlayer();
-				int locationChunk = -1;
+				int locationChunk = 0;
 				double X = targePlayer.getLocation().getX();
 				double Y = targePlayer.getLocation().getY();
-				if (X < 100 && Y < 100) {
-					locationChunk = 0;
-				}
-				if (PlayerLocationSave.playerLocationHashMap.get(targePlayer.getName()) == null) {
-					if (locationChunk == 0) {
-						new Title().send(targePlayer, "主城", ChatColor.GREEN, "相对安全的地方", ChatColor.YELLOW, 1, 3, 1);
-						PlayerLocationSave.playerLocationHashMap.put(targePlayer.getName(), 0);
-						return;
+				if (X >= 100 || X <= -100 || Y >= 100 || Y <= -100) {
+					locationChunk = 1;
+					if (X >= 200 || X <= -200 || Y >= 200 || Y <= -200) {
+						locationChunk = 2;
+						if (X >= 300 || X <= -300 || Y >= 300 || Y <= -300) {
+							locationChunk = 3;
+							if (X >= 400 || X <= -400 || Y >= 400 || Y <= -400) {
+								locationChunk = 4;
+								if (X >= 500 || X <= -500 || Y >= 500 || Y <= -500) {
+									locationChunk = 5;
+									if (X >= 600 || X <= -600 || Y >= 600 || Y <= -600) {
+										locationChunk = 6;
+										if (X >= 700 || X <= -700 || Y >= 700 || Y <= -700) {
+											locationChunk = 7;
+											if (X >= 800 || X <= -800 || Y >= 800 || Y <= -800) {
+												locationChunk = 8;
+												if (X >= 900 || X <= -900 || Y >= 900 || Y <= -900) {
+													locationChunk = 9;
+													if (X >= 1000 || X <= -1000 || Y >= 1000 || Y <= -1000) {
+														locationChunk = 10;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
 					}
 				}
 				if (PlayerLocationSave.playerLocationHashMap.get(targePlayer.getName()) == locationChunk) {
 					return;
+				}else {
+					if(locationChunk==0) {
+						new Title().send(targePlayer, "主城", ChatColor.GREEN, "相对安全的地方", ChatColor.YELLOW, 1, 3, 1);
+						PlayerLocationSave.playerLocationHashMap.put(targePlayer.getName(), 0);
+						return;
+					}
+					if(locationChunk==1) {
+						new Title().send(targePlayer, "主城郊区", ChatColor.YELLOW, "僵尸生命和攻击力乘数1.1", ChatColor.RED, 1, 3, 1);
+						PlayerLocationSave.playerLocationHashMap.put(targePlayer.getName(), 0);
+						return;
+					}
 				}
 			} else {
 				return;
